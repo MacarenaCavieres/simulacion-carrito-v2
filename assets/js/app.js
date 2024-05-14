72585b2902f73bc5ba07546c5a40c5551bf01abf
MCavieres <maca.c.91@gmail.com>
2024-05-14 14:53:36 -0400

    se agrega dataset a html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-console.log("conectado");
+const template = document.querySelector("#template").content;
+const btnAgregar = document.querySelector(".btnAgregar");
+
+btnAgregar.addEventListener("click", (e) => {
+    console.log(e.target.dataset.nombre);
+});
