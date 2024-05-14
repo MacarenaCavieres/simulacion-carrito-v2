b99bdc4c4b83b69fe9dabac60fcde22a6c615ea6
MCavieres <maca.c.91@gmail.com>
2024-05-14 17:57:17 -0400

    se realiza funcion para imprimir en el dom los articulos agregados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,50 @@
 const template = document.querySelector("#template").content;
-const btnAgregar = document.querySelector(".btnAgregar");
+const fragment = document.createDocumentFragment();
+const btnAgregar = document.querySelectorAll(".btnAgregar");
+const carritoLista = document.querySelector("#carritoLista");
+const subtitulo = document.querySelector(".subtitulo");
 
-btnAgregar.addEventListener("click", (e) => {
-    console.log(e.target.dataset.nombre);
+const arrayFrutas = [];
+
+const construirObjeto = (target) => {
+    const newFruta = {
+        nombre: target.dataset.nombre,
+        precio: +target.dataset.precio,
+        cantidad: 1,
+        total: +target.dataset.precio,
+    };
+
+    const indice = arrayFrutas.findIndex((item) => item.nombre === newFruta.nombre);
+
+    if (indice === -1) {
+        arrayFrutas.push(newFruta);
+    } else {
+        arrayFrutas[indice].cantidad++;
+        arrayFrutas[indice].total = arrayFrutas[indice].cantidad * arrayFrutas[indice].precio;
+    }
+
+    pintarFruta(arrayFrutas);
+};
+
+const pintarFruta = (arrayFrutas) => {
+    carritoLista.textContent = "";
+    arrayFrutas.forEach((item) => {
+        const clone = template.cloneNode(true);
+        clone.querySelector(".nombreFruta").textContent = item.nombre;
+        clone.querySelector(".precioFruta").textContent = `Precio: $${item.precio}`;
+        clone.querySelector(".cantidadFruta").textContent = `Cantidad: ${item.cantidad}`;
+        clone.querySelector(".totalFruta").textContent = `Total fruta: $${item.total}`;
+
+        fragment.appendChild(clone);
+    });
+
+    subtitulo.style.display = "block";
+
+    carritoLista.appendChild(fragment);
+};
+
+btnAgregar.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        construirObjeto(e.target);
+    });
 });
