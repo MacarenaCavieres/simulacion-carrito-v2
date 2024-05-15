144d374e0508ea90a9a26b2b09b72ca53d974a9b
MCavieres <maca.c.91@gmail.com>
2024-05-15 10:40:31 -0400

    se agrega funcionalidad a botones de los item

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const template = document.querySelector("#template").content;
+const templateTotal = document.querySelector("#templateTotal").content;
 const fragment = document.createDocumentFragment();
+const fragmentTotal = document.createDocumentFragment();
 const btnAgregar = document.querySelectorAll(".btnAgregar");
 const carritoLista = document.querySelector("#carritoLista");
 const subtitulo = document.querySelector(".subtitulo");
@@ -24,6 +26,7 @@ const construirObjeto = (target) => {
     }
 
     pintarFruta(arrayFrutas);
+    operacionSuma(arrayFrutas);
 };
 
 const pintarFruta = (arrayFrutas) => {
@@ -34,6 +37,23 @@ const pintarFruta = (arrayFrutas) => {
         clone.querySelector(".precioFruta").textContent = `Precio: $${item.precio}`;
         clone.querySelector(".cantidadFruta").textContent = `Cantidad: ${item.cantidad}`;
         clone.querySelector(".totalFruta").textContent = `Total fruta: $${item.total}`;
+        clone.querySelectorAll(".btnEliminar").forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                eliminar(e.target.parentNode.parentNode.parentNode.querySelector(".nombreFruta").textContent);
+            });
+        });
+
+        clone.querySelectorAll(".btnMas").forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                sumar(e.target.parentNode.parentNode.parentNode.querySelector(".nombreFruta").textContent);
+            });
+        });
+
+        clone.querySelectorAll(".btnMenos").forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                restar(e.target.parentNode.parentNode.parentNode.querySelector(".nombreFruta").textContent);
+            });
+        });
 
         fragment.appendChild(clone);
     });
@@ -48,3 +68,47 @@ btnAgregar.forEach((btn) => {
         construirObjeto(e.target);
     });
 });
+
+const operacionSuma = (arrayFrutas) => {
+    let result = 0;
+
+    arrayFrutas.forEach((item) => {
+        result += item.total;
+    });
+
+    const clone = templateTotal.cloneNode(true);
+    clone.querySelector(".totalCarrito").textContent = `Total compra: $${result}`;
+    fragmentTotal.appendChild(clone);
+
+    carritoLista.appendChild(fragmentTotal);
+};
+
+const eliminar = (target) => {
+    const oneDelete = arrayFrutas.findIndex((item) => item.nombre === target);
+    const confirmar = confirm("¿Esta seguro que quiere eliminar el item?");
+    if (confirmar) {
+        arrayFrutas.splice(oneDelete, 1);
+        pintarFruta(arrayFrutas);
+        operacionSuma(arrayFrutas);
+    }
+};
+
+const sumar = (target) => {
+    const onePlus = arrayFrutas.findIndex((item) => item.nombre === target);
+    arrayFrutas[onePlus].cantidad++;
+    arrayFrutas[onePlus].total = arrayFrutas[onePlus].cantidad * arrayFrutas[onePlus].precio;
+    pintarFruta(arrayFrutas);
+    operacionSuma(arrayFrutas);
+};
+
+const restar = (target) => {
+    const oneLess = arrayFrutas.findIndex((item) => item.nombre === target);
+    if (arrayFrutas[oneLess].cantidad > 0) {
+        arrayFrutas[oneLess].cantidad--;
+        arrayFrutas[oneLess].total = arrayFrutas[oneLess].cantidad * arrayFrutas[oneLess].precio;
+        pintarFruta(arrayFrutas);
+        operacionSuma(arrayFrutas);
+    } else {
+        eliminar(target);
+    }
+};
